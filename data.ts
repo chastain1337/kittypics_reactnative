43cdbb22946023f78d3c82e763d09b21f8421d59
James Chastain <jchastain@strategicmarketing.com>
2024-01-30 22:12:34 -0500

    s

--- data.ts
+++ data.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import { Pic } from './models';
 
-const apiUrl = process.env.NODE_ENV == "development" ? "http://192.168.1.10:5215/" : "http://kittypics-api.onrender.com/";
+const apiUrl = process.env.NODE_ENV == "development" ? "http://192.168.1.10:5215/" : "https://kittypics-api.onrender.com/";
 
 export const getRandomPics = async (count:number) : Promise<Pic[]> => {
     const res = await axios.get(`${apiUrl}Pics/GetRandomPics?count=${count}`)
